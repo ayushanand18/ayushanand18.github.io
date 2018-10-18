bdd73d2fbb854e9577a2b1bc03c300dfa703bfb8
ayushanand18 <36472216+ayushanand18@users.noreply.github.com>
2018-10-18 21:25:05 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,6 @@ const RUNTIME = 'runtime';
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   'index.html',
-  './', // Alias for index.html
   'bootstrap.min.css',
   'jquery.min.js'
 ];
