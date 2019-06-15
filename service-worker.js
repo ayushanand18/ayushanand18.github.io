a5293efea722783f93be5b87f31d3445297d4636
EC2 Default User <kingwarrior880@gmail.com>
2019-06-15 11:06:52 +0000

    Latest Updated and Bug Fixes

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@ const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-  '/wbapphstd/Scripts/jquery.min.js'
+  '/wbapphstd/Script/jquery.min.js'
 ];
 
 // The install handler takes care of precaching the resources we always need.
