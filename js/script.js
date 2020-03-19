842cba103b8441d0eb960bf94b080bf6c23ea1d1
EC2 Default User <kingwarrior880@gmail.com>
2020-03-19 14:50:16 +0000

    Fixed site issues due to JavaScript

--- js/script.js
+++ js/script.js
@@ -9,117 +9,7 @@
             $(this).remove();
         });
     });
-
-    // :: Sticky Active Code
-    $window.on('scroll', function () {
-        if ($window.scrollTop() > 0) {
-            $('.header-area').addClass('sticky');
-        } else {
-            $('.header-area').removeClass('sticky');
-        }
-    });
-
-
-    // :: Carousel Active Code
-    if ($.fn.owlCarousel) {
-        var welcomeSlider = $('.welcome_slides');
-
-        welcomeSlider.owlCarousel({
-            items: 1,
-            loop: true,
-            autoplay: true,
-            smartSpeed: 1500,
-            nav: true,
-            navText: ["<i class='ti-angle-left'</i>", "<i class='ti-angle-right'</i>"],
-            dots: true,
-            animateIn: 'fadeIn',
-            animateOut: 'fadeOut'
-        })
-        welcomeSlider.on('translate.owl.carousel', function () {
-            var layer = $("[data-animation]");
-            layer.each(function () {
-                var anim_name = $(this).data('animation');
-                $(this).removeClass('animated ' + anim_name).css('opacity', '0');
-            });
-        });
-        $("[data-delay]").each(function () {
-            var anim_del = $(this).data('delay');
-            $(this).css('animation-delay', anim_del);
-        });
-        $("[data-duration]").each(function () {
-            var anim_dur = $(this).data('duration');
-            $(this).css('animation-duration', anim_dur);
-        });
-        welcomeSlider.on('translated.owl.carousel', function () {
-            var layer = welcomeSlider.find('.owl-item.active').find("[data-animation]");
-            layer.each(function () {
-                var anim_name = $(this).data('animation');
-                $(this).addClass('animated ' + anim_name).css('opacity', '1');
-            });
-        });
-
-        $(".client_slides").owlCarousel({
-            items: 1,
-            loop: true,
-            autoplay: true,
-            smartSpeed: 700,
-            dots: true
-        });
-
-        var dot = $('.client_slides .owl-dot');
-        dot.each(function () {
-            var index = $(this).index() + 1;
-            if (index < 10) {
-                $(this).html('0').append(index);
-            } else {
-                $(this).html(index);
-            }
-        });
-    }
-
-    // :: Magnific-popup Video Active Code
-    if ($.fn.magnificPopup) {
-        $('#videobtn').magnificPopup({
-            type: 'iframe'
-        });
-        $('.gallery_img').magnificPopup({
-            type: 'image',
-            gallery: {
-                enabled: true
-            },
-            removalDelay: 300,
-            mainClass: 'mfp-fade',
-            preloader: true
-        });
-    }
-
-    // :: Masonary Gallery Active Code
-    if ($.fn.imagesLoaded) {
-        $('.dream-portfolio').imagesLoaded(function () {
-            // filter items on button click
-            $('.portfolio-menu').on('click', 'button', function () {
-                var filterValue = $(this).attr('data-filter');
-                $grid.isotope({
-                    filter: filterValue
-                });
-            });
-            // init Isotope
-            var $grid = $('.dream-portfolio').isotope({
-                itemSelector: '.single_gallery_item',
-                percentPosition: true,
-                masonry: {
-                    columnWidth: '.single_gallery_item'
-                }
-            });
-        });
-    }
-
-    // :: Gallery Menu Style Active Code
-    $('.portfolio-menu button.btn').on('click', function () {
-        $('.portfolio-menu button.btn').removeClass('active');
-        $(this).addClass('active');
-    })
-
+    
     // :: ScrollUp Active Code
     if ($.fn.scrollUp) {
         $.scrollUp({
@@ -127,16 +17,7 @@
             scrollText: 'Scroll Top'
         });
     }
-
-    // :: onePageNav Active Code
-    if ($.fn.onePageNav) {
-        $('#nav').onePageNav({
-            currentClass: 'active',
-            scrollSpeed: 1500,
-            easing: 'easeOutQuad'
-        });
-    }
-
+    
     // :: CounterUp Active Code
     if ($.fn.counterUp) {
         $('.counter').counterUp({
@@ -162,15 +43,6 @@
         $.preventDefault();
     });
 
-    // :: Accordian Active Code
-    (function () {
-        var dd = $('dd');
-        dd.filter(':nth-child(n+3)').hide();
-        $('dl').on('click', 'dt', function () {
-            $(this).next().slideDown(500).siblings('dd').slideUp(500);
-        })
-    })();
-
     // :: Nav Active Code
     if ($.fn.classyNav) {
         $('#dreamNav').classyNav({
@@ -182,13 +54,5 @@
     if ($.fn.niceScroll) {
         $(".timelineBody").niceScroll();
     }
-
-    $('.simple_timer').syotimer({
-        year: 2020,
-        month: 9,
-        day: 9,
-        hour: 20,
-        minute: 30
-    })
 
 })(jQuery);
