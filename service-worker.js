29452afb2d8c6d56ed509ddeba7a6f40638356d2
EC2 Default User <kingwarrior880@gmail.com>
2020-03-20 10:51:52 +0000

    Update: used jquery and bootstrap cdn . plus removed unused css and javascript

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@ const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-  'js/jquery.min.js'
+  'js/script.js'
 ];
 
 // The install handler takes care of precaching the resources we always need.
